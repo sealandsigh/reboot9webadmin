c11a0fb69971912170719e32cf42d1a8456fa19a
sealandsigh <zhujiajunzhu@qq.com>
2021-08-09 22:14:15 +0800

    调整es集群管理的编辑删除按钮位置

--- src/views/esmanager/escluster/table.vue
+++ src/views/esmanager/escluster/table.vue
@@ -61,7 +61,7 @@
         prop="password">
       </el-table-column>
 
-      <el-table-column label="操作">
+      <el-table-column label="操作" width="200">
         <template slot-scope="scope">
           <el-button
             size="mini"
